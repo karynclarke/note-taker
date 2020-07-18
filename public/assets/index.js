028c49487f148272491f7b04b70f73c7eff1f723
karynclarke <karynclarke@comcast.net>
2020-07-17 19:18:38 -0700

    stop spinning

--- public/assets/index.js
+++ public/assets/index.js
@@ -2,7 +2,7 @@ const $noteTitle = $(".note-title");
 const $noteText = $(".note-textarea");
 const $saveNoteBtn = $(".save-note");
 const $newNoteBtn = $(".new-note");
-const $noteList = $(".list-container .list-group");
+const $noteList = $(".list-container.list-group");
 
 // activeNote is used to keep track of the note in the textarea
 let activeNote = {};
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,16 +1,19 @@
 var path = require("path");
-module.exports = function(app) {
-    // HTML GET Requests
+const router = require("express").Router();
+// module.exports = function(app) 
+// HTML GET Requests
 
-    app.get("/index", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    });
+router.get("/index", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
-    app.get("/notes", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/notes.html"));
-    });
+router.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
 
-    app.get("*", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    });
-};
+router.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+
+module.exports = router;
